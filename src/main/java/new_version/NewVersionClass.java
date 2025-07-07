fcbd795a12d494a53ed424ae87ea2ada5fe4418d
Zhanarkhan <zhanarkhan.utemisov@gmail.com>
2025-07-07 11:05:10 +0600

    new version 4.12.4

--- src/main/java/new_version/NewVersionClass.java
+++ src/main/java/new_version/NewVersionClass.java
@@ -2,6 +2,6 @@ package new_version;
 
 public class NewVersionClass {
     public static void main(String[] args) {
-        System.out.println("New Version 4.12.2");
+        System.out.println("New Version 4.12.4");
     }
 }
