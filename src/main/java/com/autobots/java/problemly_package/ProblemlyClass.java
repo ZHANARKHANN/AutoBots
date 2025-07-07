de141612023fa96c08fba425b6d916ab48c98b99
Zhanarkhan <zhanarkhan.utemisov@gmail.com>
2025-07-07 17:14:52 +0600

    first commit

---
+++ src/main/java/com/autobots/java/problemly_package/ProblemlyClass.java
@@ -0,0 +1,10 @@
+package com.autobots.java.problemly_package;
+
+public class ProblemlyClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Everything is ok");
+
+    }
+}
